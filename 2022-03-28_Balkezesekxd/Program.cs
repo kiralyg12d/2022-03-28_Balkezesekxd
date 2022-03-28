2c5e5defc356df3bbb815dbe62fed92c0bde9718
kiralyg12d <95190713+kiralyg12d@users.noreply.github.com>
2022-03-28 10:34:05 +0200

    valami

--- 2022-03-28_Balkezesekxd/Program.cs
+++ 2022-03-28_Balkezesekxd/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace _2022_03_28_Balkezesekxd
@@ -10,6 +11,12 @@ namespace _2022_03_28_Balkezesekxd
     {
         static void Main(string[] args)
         {
+            List<Sportolok> sportoloks = new List<Sportolok>();
+            foreach (var sor in File.ReadAllLines("baélezesek.csv").Skip(1))
+            {
+                sportoloks.Add(new Sportolok(sor));
+            }
+            Console.WriteLine($"3.feladat:{sportoloks.Count}");
         }
     }
 }
---
+++ 2022-03-28_Balkezesekxd/Sportolok.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_03_28_Balkezesekxd
+{
+    class Sportolok
+    {
+        public string NÉV { get; set; }
+
+        public string Első { get; set; }
+
+        public string Utolsó { get; set; }
+
+        public int Súly { get; set; }
+
+        public int Magasság { get; set; }
+    
+        public Sportolok (string sor)
+        {
+            string[] t = sor.Split(';');
+            NÉV = t[0];
+            Első = t[1];
+            Utolsó = t[2];
+            Súly = int.Parse(t[3]);
+            Magasság = int.Parse(t[4]);
+        }
+    }
+}
